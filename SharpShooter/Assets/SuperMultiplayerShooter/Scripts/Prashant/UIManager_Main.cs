ce2bbcc7a19b8883e9e7c589a63205fdcbacc54e
prashantpayiza <prashant@payizatech.com>
2022-08-24 09:08:32 +0530

    character selection new UI function added

--- SharpShooter/Assets/SuperMultiplayerShooter/Scripts/Prashant/UIManager_Main.cs
+++ SharpShooter/Assets/SuperMultiplayerShooter/Scripts/Prashant/UIManager_Main.cs
@@ -1,18 +1,48 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager_Main : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private GameObject characterScreenPanel;
+    [SerializeField]
+    private InputField playerName;
+    [SerializeField]
+    private Button nextButton;
+
+    [SerializeField]
+    private GameObject mainPanel;
+
+
+    private void Start()
     {
+        nextButton.onClick.AddListener(OnNextButtonClick);
+
+        if (PlayerPrefs.HasKey(StaticData.characterSelected))
+        {
+            if (PlayerPrefs.GetInt(StaticData.characterSelected) == 0)
+            {
+                Debug.Log("character Not Selected yet");
+            }
+            else
+            {
+                characterScreenPanel.SetActive(false);
+                mainPanel.SetActive(true);
+            }
+        }
         
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnNextButtonClick()
     {
-        
+        characterScreenPanel.SetActive(false);
+        mainPanel.SetActive(true);
+
+        PlayerPrefs.SetInt(StaticData.characterSelected, 1);
+
     }
 }
